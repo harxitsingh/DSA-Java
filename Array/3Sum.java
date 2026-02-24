459fd3d722dba7b980f9dd03a88a124fd4e58b2b
Harshit Singh <harxitsingh@gmail.com>
2026-02-24 20:56:44 +0530

    Implement threeSum method for finding triplets

--- Array/3Sum.java
+++ Array/3Sum.java
@@ -1,2 +1,44 @@
-l
-  
+//Leetcode question no - 
+// Approach - sort + two pointer
+import java.util.*;
+
+public class Solution {
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int j = i + 1, k = nums.length - 1;
+
+            while (j < k) {
+
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if (sum < 0) j++;
+                else if (sum > 0) k--;
+                else {
+                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+
+                    while (j < k && nums[j] == nums[j - 1]) j++;
+                    while (j < k && nums[k] == nums[k + 1]) k--;
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+
+        System.out.println(threeSum(nums));
+    }
+}
