537be784713f67e1b08b35807e938f23bf126ab7
Harshit Singh <harxitsingh@gmail.com>
2026-02-27 22:29:45 +0530

    Update Main.java

--- Array/Main.java
+++ Array/Main.java
@@ -1,22 +1,20 @@
-// import java.util.*;
+ import java.util.*;
 
-// class Solution {
-//     public int[] twoSum(int[] nums, int target) {
-//         Map<Integer, Integer> map = new HashMap<>(); // value -> index
+ class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>(); // value -> index
+     for (int i = 0; i < nums.length; i++) {
+             int need = target - nums[i];
+           if (map.containsKey(need)) {
+               return new int[] { map.get(need), i };
+           }
+             map.put(nums[i], i);        }
 
-//         for (int i = 0; i < nums.length; i++) {
-//             int need = target - nums[i];
-//             if (map.containsKey(need)) {
-//                 return new int[] { map.get(need), i };
-//             }
-//             map.put(nums[i], i);
-//         }
-
-//         // Guaranteed to exist so this never hits
-//         return new int[0];
-//     }
-// }
-public class Main {
+        // Guaranteed to exist so this never hits
+       return new int[0];
+    }
+ }
+public class Main 
     public static void main(String[] args) {
         Solution s = new Solution();
         int[] arr = {1, 2, 9};
@@ -25,3 +23,4 @@ public class Main {
         for (int x : ans) System.out.print(x + " ");
     }
 }
+
