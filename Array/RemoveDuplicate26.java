76fbe36fc29e935db85f423130cad3b8004c5e66
Harshit Singh <harxitsingh@gmail.com>
2026-03-01 21:24:19 +0530

    Refactor Solution class to RemoveDuplicate26 class

--- Array/RemoveDuplicate26.java
+++ Array/RemoveDuplicate26.java
@@ -1,19 +1,27 @@
 // LeetCode Question 26 = Remove Duplicates from Sorted Array.
-class Solution {
-    public int removeDuplicates(int[] nums) {
+public class RemoveDuplicate26 {
+    public static int removeDuplicates(int[] nums) {
 
         if (nums.length == 0)
             return 0;
-
-        int k = 0;
-
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] != nums[k]) {
-                k++;
-                nums[k] = nums[i];
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
             }
         }
 
-        return k + 1;
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,1,2,2,3};
+        int k = removeDuplicates(nums);
+        System.out.println("Number of unique elements: " + k);
+
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
     }
 }
