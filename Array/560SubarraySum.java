851a989805eabfd5b3a12abc99306f3eeed718cb
Harshit Singh <harxitsingh@gmail.com>
2026-03-02 18:50:38 +0530

    Add brute force solution for subarray sum problem
    Implemented a brute force approach to count subarrays with a given sum.

--- Array/560SubarraySum.java
+++ Array/560SubarraySum.java
@@ -1,1 +1,26 @@
 // Leetcode q no 560
+//Approach - Brute force
+//Time complexity - 0(n^2) not 
+
+public class SubarraySum {
+    // Function to count subarrays with sum = k
+    public static int subarraySum(int[] nums, int k) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int sum = 0;
+           for (int j = i; j < nums.length; j++) {
+                sum = sum + nums[j];
+                if (sum == k) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 1};
+        int k = 2;
+        int result = subarraySum(nums, k);
+        System.out.println("Answer: " + result);
+    }
+}
