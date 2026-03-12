2c6b54b7a957334cd6465ecd04c0aa507cb5f4e4
Harshit Singh <harxitsingh@gmail.com>
2026-03-12 18:22:09 +0530

    Add method to find shortest unsorted subarray
    Implemented a method to find the length of the shortest unsorted subarray in an array of integers.

--- Array/LC581_ShortestUnsortedSubarray.java
+++ Array/LC581_ShortestUnsortedSubarray.java
@@ -1,1 +1,47 @@
-//technique 
+public class LC581_ShortestUnsortedSubarray {
+
+    public static int findUnsortedSubarray(int[] nums) {
+
+        int n = nums.length;
+        int left = -1;
+        int right = -1;
+
+        int max = nums[0];
+        int min = nums[n - 1];
+
+        // Left → Right traversal
+        for (int i = 1; i < n; i++) {
+
+            if (nums[i] < max) {
+                right = i;
+            } else {
+                max = nums[i];
+            }
+        }
+
+        // Right → Left traversal
+        for (int i = n - 2; i >= 0; i--) {
+
+            if (nums[i] > min) {
+                left = i;
+            } else {
+                min = nums[i];
+            }
+        }
+
+        if (right == -1) {
+            return 0;
+        }
+
+        return right - left + 1;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {2, 6, 4, 8, 10, 9, 15};
+
+        int result = findUnsortedSubarray(nums);
+
+        System.out.println("Length of shortest unsorted subarray: " + result);
+    }
+}
