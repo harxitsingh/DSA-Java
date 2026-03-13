3d03e0cf2d7f41efabaf408261bab54fcb2c74a6
Harshit Singh <harxitsingh@gmail.com>
2026-03-13 18:31:53 +0530

    Fix formatting issues in LC344_ReverseString.java

--- String/LC344_ReverseString.java
+++ String/LC344_ReverseString.java
@@ -1,12 +1,9 @@
 public class LC344_ReverseString {
-
     public static void reverseString(char[] s) {
-
         int left = 0;
         int right = s.length - 1;
 
         while (left < right) {
-
             char temp = s[left];
             s[left] = s[right];
             s[right] = temp;
@@ -17,13 +14,11 @@ public class LC344_ReverseString {
     }
 
     public static void main(String[] args) {
-
         char[] s = {'h','e','l','l','o'};
-
         reverseString(s);
-
         for (char c : s) {
             System.out.print(c + " ");
         }
     }
+
 }
